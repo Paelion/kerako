c17fb96f8c38ae7add8bbbbce095765672401cce
Paelion <aelion.pauline1@gmail.com>
2019-12-19 23:18:38 +0100

    add style histoire.php

--- kerako/wp-content/themes/themeKerako/page-histoire.php
+++ kerako/wp-content/themes/themeKerako/page-histoire.php
@@ -9,32 +9,57 @@ get_header();
 if (!function_exists('get_field')) return;
 ?>
 
+    <div class="content">
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main">
+                <div class="atelier">
+                    <div class="atelierTitle">
+                        <h4>Mon atelier</h4>
+                        <hr class="atelier--line">
+                    </div>
+
+                    <h5>Atelier Kerako</h5>
+                    <div class="atelier--flex">
+                        <p><?php the_field('atelier_desc'); ?></p>
+                        <img class="imageAtelier" src="<?php the_field('atelier_img') ?>">
+                    </div>
 
 
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main">
-            <div class="portrait">
-                <h4>Mon histoire</h4>
-                <hr class="portrait--line">
-                <img class="imagePortrait" src="<?php the_field('portrait_img')?>">
-                <h2>Elodie Hoerter</h2>
-                <p><?php the_field('portrait_desc'); ?></p>
-                <button type="button" class="btn--portrait">Me contacter</button>
-            </div>
-            <div class="atelier">
-                <h4>Mon atelier</h4>
-                <hr class="portrait--line">
-                <h5>Atelier Kerako</h5>
-                <p><?php the_field('atelier_desc'); ?></p>
-                <img class="imagePortrait" src="<?php the_field('atelier_img')?>">
-            </div>
-            <img src="<?php the_field('atelier_img')?>">
+                </div>
 
+                <div class="portrait">
+                    <div class="portraitTitle">
+                        <h4>Notre histoire</h4>
+                        <hr class="portrait--line">
+                    </div>
+                    <div class="portrait--flex">
+                        <img class="imagePortrait" src="<?php the_field('portrait_img') ?>">
+                        <div class="portrait--block">
+                            <h2>Elodie Hoerter</h2>
+                            <p><?php the_field('portrait_desc'); ?></p>
+                            <button type="button" class="btn--portrait">Me contacter</button>
+                        </div>
+                    </div>
+                </div>
 
+                <img class="separateurImg" src="<?php the_field('separateur_img') ?>">
 
+                <div class="blog">
+                    <h2>Notre blog</h2>
+                    <button type="button" class="btn--blog">Cliquez ici</button>
+                </div>
 
+                <div class="histoireInfo histoireInfo1 ">
+                    <p><?php the_field('histoire_info_1'); ?></p>
+                    <img class="histoireInfoImg" src="<?php the_field('histoire_info_img_1') ?>">
+                </div>
 
-        </main>
+                <div class="histoireInfo">
+                    <img class="histoireInfoImg" src="<?php the_field('histoire_info_img_2') ?>">
+                    <p><?php the_field('histoire_info_2'); ?></p>
+                </div>
+            </main>
+        </div>
     </div>
 
 <?php
