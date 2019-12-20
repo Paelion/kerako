569bb64ff198a314d7c36edc85744f9710536f55
Paelion <aelion.pauline1@gmail.com>
2019-12-20 17:52:18 +0100

    add style page compte client

--- kerako/wp-content/themes/themeKerako/page-client-compte.php
+++ kerako/wp-content/themes/themeKerako/page-client-compte.php
@@ -10,13 +10,18 @@ if (!function_exists('get_field')) return;
 ?>
 
 
-
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main">
-            <div class="promo"><?php the_field('table'); ?></div><br>
-            <div><?php the_field('table_img'); ?></div>
-        </main><!-- #main -->
-    </div><!-- #primary -->
+    <div class="content">
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main compte">
+                <h4>Mon compte</h4>
+                <div class="wrap--button">
+                    <button type="button" class="btn--compte btn--compte1">Mes informations</button>
+                    <button type="button" class="btn--compte btn--compte2">Mes commandes</button>
+                    <button type="button" class=" btn--compte btn--compte3">Mes favoris</button>
+                </div>
+            </main>
+        </div>
+    </div>
 
 <?php
 get_footer();
