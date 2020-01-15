44f93275d6191e0d90c45da10a5fa394736a622a
Paelion <aelion.pauline1@gmail.com>
2020-01-15 17:21:01 +0100

    modif header + hover

--- kerako/wp-content/themes/themeKerako/header.php
+++ kerako/wp-content/themes/themeKerako/header.php
@@ -10,26 +10,32 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,700&display=swap" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-    <title><?php wp_title();  ?></title>
+    <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
 
 </head>
 <body <?php body_class(); ?>>
-    <div id="container">
-        <header>
-            <img src="<?php echo get_template_directory_uri(); ?>/images/logoKerako.jpg" class="logoKerako">
+<div id="container">
+    <header>
+        <img src="<?php echo get_template_directory_uri(); ?>/images/logoKerako.jpg" class="logoKerako">
+                <div class="header--right">
             <div class="formSearch">
                 <form action="/kerako" method="get" class="searchForm">
-                    <input type="text" name="s" value="<?php the_search_query(); ?>" placeholder="Recherche" />
-                    <button type="submit" class="button-search"><img src="<?php echo get_template_directory_uri(); ?>/images/loupe.png" class="loupe"></button>
+                    <input type="text" name="s" value="<?php the_search_query(); ?>" placeholder="Recherche"/>
+                    <button type="submit" class="button-search"><img
+                                src="<?php echo get_template_directory_uri(); ?>/images/loupe.png" class="loupe">
+                    </button>
                 </form>
-                <hr class="bottom--search">
             </div>
             <div class="accesPage">
-                <h5><a href="http://localhost:8888/kerako/mon-compte/">Votre compte</a></h5>
-                <h5><a href="http://localhost:8888/kerako/panier-2/">Shop</a></h5>
+                <a href="http://localhost:8888/kerako/panier-2/"><img
+                            src="<?php echo get_template_directory_uri(); ?>/images/accueil/shop2.png" class="logoShop"></a>
+                <a href="http://localhost:8888/kerako/mon-compte/"><img
+                            src="<?php echo get_template_directory_uri(); ?>/images/accueil/profil2.png"
+                            class="logoProfil"></a>
             </div>
-        </header>
-        <nav class="menu--nav">
-            <?php wp_nav_menu(array('theme_location' => 'primary_menu')); ?>
-        </nav>
+        </div>
+    </header>
+    <nav class="menu--nav">
+        <?php wp_nav_menu(array('theme_location' => 'primary_menu')); ?>
+    </nav>
